7190ee7dab241208b7d23f8f3b66bbf451232d3d
gali1998 <gali1998@gmail.com>
2020-08-12 22:30:06 +0300

    try

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 // create express app
 const app = express();
-
+const port = process.env.PORT || 3000;
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -35,6 +35,6 @@ app.get('/', (req, res) => {
 require('./routes/result.routes')(app);
 
 // listen for requests
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is listening on port 3000");
 });
